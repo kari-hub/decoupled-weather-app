b10e29e478a06922436336f65ab0eeeb35ef58d6
kari-hub <karilaura77@gmail.com>
2025-04-25 15:37:17 +0300

    improved weather API endpoints and configuration

--- weather-api/routes/api.php
+++ weather-api/routes/api.php
@@ -3,8 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WeatherController;
+use App\Http\Controllers\GeocodeController;
 
 Route::get('/weather', [WeatherController::class, 'getCurrentWeather']);
 Route::get('/forecast', [WeatherController::class, 'getForecast']);
 Route::get('/weather/{city}', [WeatherController::class, 'getWeatherByCity']);
+Route::get('/geocode', [GeocodeController::class, 'getCoordinates']);
 // Route::get('/weather/coordinates', [WeatherController::class, 'getWeatherByCoordinates']);
